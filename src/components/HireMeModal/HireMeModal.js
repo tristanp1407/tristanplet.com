04c6f6c5fcdcc4b128da0d2bafe2ea99953d9af2
tristanp1407 <63342617+tristanp1407@users.noreply.github.com>
2024-03-15 09:04:30 -0600

    Update number to MX

--- src/components/HireMeModal/HireMeModal.js
+++ src/components/HireMeModal/HireMeModal.js
@@ -113,7 +113,7 @@ const HireMeModal = ({ open, setOpen }) => {
             <IconButton
               {...iconButtonProps}
               onClick={() =>
-                window.open("https://wa.me/447807462252", "_blank")
+                window.open("https://wa.me/+525539381634", "_blank")
               }
             >
               <FaWhatsapp size="50px" />
